19fe0c3aa619b4ae518bd0eec5a7a81396ef6961
triple666killaz <blackrascal14@gmail.com>
2023-04-18 18:08:30 +0300

    Added CLoudinary image upload service

--- RunGroupWebApp/Controllers/ClubController.cs
+++ RunGroupWebApp/Controllers/ClubController.cs
@@ -4,16 +4,19 @@ using Microsoft.EntityFrameworkCore;
 using RunGroupWebApp.Data;
 using RunGroupWebApp.Interfaces;
 using RunGroupWebApp.Models;
+using RunGroupWebApp.ViewModels;
 
 namespace RunGroupWebApp.Controllers;
 
 public class ClubController : Controller
 {
     private readonly IClubRepository _clubRepository;
+    private readonly IPhotoService _photoService;
 
-    public ClubController(IClubRepository clubRepository)
+    public ClubController(IClubRepository clubRepository, IPhotoService photoService)
     {
         _clubRepository = clubRepository;
+        _photoService = photoService;
     }
     
     public async Task<IActionResult> Index()
@@ -36,14 +39,30 @@ public class ClubController : Controller
     }
 
     [HttpPost]
-    public async Task<IActionResult> Create(Club club)
+    public async Task<IActionResult> Create(CreateClubViewModel clubVM)
     {
-        if (!ModelState.IsValid)
+        if (ModelState.IsValid)
         {
-            return View(club);
-        }
+            var result = await _photoService.AddPhotoAsync(clubVM.Image);
 
-        _clubRepository.Add(club);
-        return RedirectToAction("Index");
+            var club = new Club()
+            {
+                Title = clubVM.Title,
+                Description = clubVM.Description,
+                Image = result.Url.ToString(),
+                Address = new Address()
+                {
+                    City = clubVM.Address.City,
+                    State = clubVM.Address.State,
+                    Street = clubVM.Address.Street
+                }
+            };
+            
+            _clubRepository.Add(club);
+            return RedirectToAction("Index");
+        }
+        
+        ModelState.AddModelError("", "Photo upload failed");
+        return View(clubVM);
     }
 }
--- RunGroupWebApp/Controllers/RaceController.cs
+++ RunGroupWebApp/Controllers/RaceController.cs
@@ -1,17 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using RunGroupWebApp.Data;
 using RunGroupWebApp.Interfaces;
+using RunGroupWebApp.Models;
+using RunGroupWebApp.ViewModels;
 
 namespace RunGroupWebApp.Controllers;
 
 public class RaceController : Controller
 {
     private readonly IRaceRepository _raceRepository;
+    private readonly IPhotoService _photoService;
 
-    public RaceController(IRaceRepository raceRepository)
+    public RaceController(IRaceRepository raceRepository, IPhotoService photoService)
     {
         _raceRepository = raceRepository;
+        _photoService = photoService;
     }
     
     public async Task<IActionResult> Index()
@@ -27,4 +32,39 @@ public class RaceController : Controller
 
         return View(race);
     }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateRaceViewModel raceVM)
+    {
+        if (ModelState.IsValid)
+        {
+            var result = await _photoService.AddPhotoAsync(raceVM.Image);
+
+            var race = new Race()
+            {
+                Title = raceVM.Title,
+                Description = raceVM.Description,
+                Image = result.Url.ToString(),
+                Address = new Address()
+                {
+                    City = raceVM.Address.City,
+                    State = raceVM.Address.State,
+                    Street = raceVM.Address.Street
+                }
+            };
+            
+            _raceRepository.Add(race);
+            return RedirectToAction("Index");
+        }
+        
+        ModelState.AddModelError("", "Photo upload failed");
+        return View(raceVM);
+
+    }
+
 }
